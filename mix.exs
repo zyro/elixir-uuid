bebccc2997ca708e720d096510b2f020c816d859
Andrei Mihu <andrei.mihu@gmx.com>
2014-06-07 00:44:31 +0100

    Initial version.

---
+++ mix.exs
@@ -0,0 +1,21 @@
+defmodule UUID.Mixfile do
+  use Mix.Project
+
+  def project do
+    [ app: :uuid,
+      version: "0.1.0",
+      elixir: "== 0.13.3 or ~> 0.14.0-dev",
+      deps: deps ]
+  end
+
+  # Application configuration.
+  def application do
+    []
+  end
+
+  # List of dependencies.
+  defp deps do
+    []
+  end
+
+end
