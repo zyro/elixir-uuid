2c1618cf8c1c8fab0ca467425fea5f1c86a251b7
Andrew King <me@andrewsking.com>
2016-09-28 15:36:08 +1000

    Add "UUID.info/1" function
    * Does not raise exception.
    * Returns `{:ok, information}` or `{:error, messages}`.

--- test/uuid_test.exs
+++ test/uuid_test.exs
@@ -2,6 +2,14 @@ defmodule UUIDTest do
   use ExUnit.Case
 
   test "UUID.info/1 invalid argument type" do
+    assert UUID.info(:not_a_uuid) == {:error, "Invalid argument; Expected: String"}
+  end
+
+  test "UUID.info/1 invalid UUID" do
+    assert UUID.info("not_a_uuid") == {:error, "Invalid argument; Not a valid UUID: not_a_uuid"}
+  end
+
+  test "UUID.info!/1 invalid argument type" do
     assert_raise(
       ArgumentError,
       "Invalid argument; Expected: String",
@@ -11,7 +19,7 @@ defmodule UUIDTest do
     )
   end
 
-  test "UUID.info/1 invalid UUID" do
+  test "UUID.info!/1 invalid UUID" do
     assert_raise(
       ArgumentError,
       "Invalid argument; Not a valid UUID: not_a_uuid",
@@ -22,14 +30,27 @@ defmodule UUIDTest do
   end
 
   # Expand the lines in info_tests.txt into individual tests for the
+  # UUID.info!/1 function, assuming the lines are:
+  #   test name || expected output || input value
+  for line <- File.stream!(Path.join([__DIR__, "info_tests.txt"]), [], :line) do
+    [name, expected, input] =
+      line |> String.split("||") |> Enum.map(&String.strip(&1))
+    test "UUID.info!/1 #{name}" do
+      {expected, []} = Code.eval_string(unquote(expected))
+      result = UUID.info!(unquote(input))
+      assert ^expected = result
+    end
+  end
+
+  # Expand the lines in info_tests.txt into individual tests for the
   # UUID.info/1 function, assuming the lines are:
   #   test name || expected output || input value
   for line <- File.stream!(Path.join([__DIR__, "info_tests.txt"]), [], :line) do
     [name, expected, input] =
       line |> String.split("||") |> Enum.map(&String.strip(&1))
-    test name do
+    test "UUID.info/1 #{name}" do
       {expected, []} = Code.eval_string(unquote(expected))
-      result = UUID.info!(unquote(input))
+      {:ok, result} = UUID.info(unquote(input))
       assert ^expected = result
     end
   end
