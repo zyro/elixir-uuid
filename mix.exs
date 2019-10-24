346581c7e89872e0e263e10f079e566cf1fc3a68
Andrei Mihu <and.mihu@gmail.com>
2019-10-24 14:33:58 +0100

    Update ex_doc dependency to 0.19

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule UUID.Mixfile do
 
   # List of dependencies.
   defp deps do
-    [{:ex_doc, "~> 0.16", only: :dev},
+    [{:ex_doc, "~> 0.19", only: :dev},
      {:earmark, "~> 1.2", only: :dev},
      {:benchfella, "~> 0.3", only: :dev}]
   end
