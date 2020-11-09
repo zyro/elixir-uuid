1da1e61061bc031e0f9b0922311b55d8484528d2
Eduardo Cunha <eduardo_cunha@bose.com>
2020-11-09 14:40:28 +0000

    Update dependencies

--- mix.exs
+++ mix.exs
@@ -24,8 +24,8 @@ defmodule UUID.Mixfile do
 
   # List of dependencies.
   defp deps do
-    [{:ex_doc, "~> 0.19", only: :dev},
-     {:earmark, "~> 1.2", only: :dev},
+    [{:ex_doc, "~> 0.23", only: :dev},
+     {:earmark, "~> 1.4", only: :dev},
      {:benchfella, "~> 0.3", only: :dev}]
   end
 
